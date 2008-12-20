760ef2e22682fcb34df8ec54da2f8db574426b93
Hackadelic <Hackadelic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-20 16:31:38 +0000

    tagged version 1.0rc5
    git-svn-id: https://plugins.svn.wordpress.org/hackadelic-sliding-notes/tags/1.0rc5@81374 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ hackadelic-sliders.php
@@ -0,0 +1,141 @@
+<?php 
+/*
+Plugin Name: Hackadelic Sliding Notes
+Version: 1.0rc5
+Plugin URI: http://hackadelic.com/solutions/wordpress/sliding-notes
+Description: Ajax sliders for content fragments
+Author: Hackadelic
+Author URI: http://hackadelic.com
+*/
+
+add_action('wp_print_scripts', 'hackadelic_sliders_scripts');
+add_action('wp_footer', 'hackadelic_print_init_js');
+add_filter('the_content', 'hackadelic_sliders_prefilter', 5);
+add_filter('the_content', 'hackadelic_sliders_postfilter', 55);
+add_shortcode('slider', 'hackadelic_shortcode_slider');
+add_shortcode('slider_usage', 'hackadelic_shortcode_slider_usage');
+
+//---------------------------------------------------------------------------------------------
+
+$sliderID = 0;
+$priorID = 0;
+
+//---------------------------------------------------------------------------------------------
+
+function hackadelic_sliders_scripts() {
+	wp_enqueue_script('jquery');
+}
+
+//---------------------------------------------------------------------------------------------
+
+function hackadelic_print_init_js() {
+?>
+<script>
+jQuery(document).ready(function() {
+	jQuery('div.hackadelic-sliderPanel').show().hide();
+});
+</script>
+<?php
+}
+
+//---------------------------------------------------------------------------------------------
+
+function hackadelic_sliders_prefilter($content) {
+	global $sliderID, $priorID;
+	$priorID = $sliderID;
+	return $content;
+}
+
+//---------------------------------------------------------------------------------------------
+
+function hackadelic_sliders_postfilter($content) {
+	global $sliderID, $priorID;
+	if ($sliderID == $priorID) return $content;
+	return preg_replace('@<p>(.+?)</p>@s', '\1<p></p>', $content);
+}
+
+//---------------------------------------------------------------------------------------------
+
+function hackadelic_shortcode_slider_usage($atts, $content=null) {
+	return '<code>'
+		.'[slider title=&quot;<em>slider button title</em>&quot;]'
+		.'<em>sliding note content</em>[/slider]</code>'
+		;
+}
+
+//---------------------------------------------------------------------------------------------
+
+function hackadelic_shortcode_slider($atts, $content=null) {
+	extract(shortcode_atts(array(
+		'title' => '+/-',
+		), $atts ));
+
+	global $sliderID;
+	++$sliderID;
+
+	$clickCode = "jQuery('#hackadelic-sliderPanel-$sliderID').slideToggle('fast')";
+
+	$panelTag = 'div'; //preg_match('/\<\/?(p)>/i', $content) ? 'div' : 'span';
+
+	if ($notestyle) $notestyle = ' style="'.$notestyle.'"';
+
+	$content = ''
+		.'<a href="javascript:;" class="hackadelic-sliderButton" '
+		."title=\"expand/collapse slider: $title\""
+		."onclick=\"$clickCode\" title=\"expand/collapse slider: $title.\">$title&raquo;</a>"
+		."<$panelTag class=\"hackadelic-sliderPanel $noteclass\" id=\"hackadelic-sliderPanel-$sliderID\"$notestyle>$content"
+		."</$panelTag>"
+		;
+	return $content;
+}
+
+// ===========================================================================
+// Admin Pages
+// ===========================================================================
+
+add_action('admin_menu', 'hackadelic_sliders_addAdminMenu');
+
+//---------------------------------------------------------------------------------------------
+
+$pluginTitle = 'Hackadelic Sliding Notes';
+
+//---------------------------------------------------------------------------------------------
+
+function hackadelic_sliders_addAdminMenu() {
+  global $pluginTitle;
+  add_options_page(
+    $pluginTitle, $pluginTitle, 10, 
+    __FILE__, 'hackadelic_sliders_displayAdminPage');
+}
+
+//---------------------------------------------------------------------------------------------
+
+function hackadelic_sliders_displayAdminPage() {
+global $pluginTitle;
+?>
+<div class="wrap">
+<h2><?php echo $pluginTitle ?></h2>
+<p>
+To customize the look of your sliding notes,
+integrate the
+<a href="http://hackadelic.com/solutions/wordpress/sliding-notes#sample-css">sample CSS code</a>
+from the
+<a href="http://hackadelic.com/solutions/wordpress/sliding-notes">plugin homepage</a>
+into your stylesheet(s), and adjust it as you see fit.
+</p>
+<p>
+If you don't want to mess around with your theme's <code>style.css</code> file,
+consider using the <a href="http://wordpress.org/extend/plugins/mycss/">MyCSS plugin</a>,
+which will enable you to add/edit extra CSS definitions
+from within the WordPress admin interface.
+</p>
+<p>
+Done that? Well then, that's it! 
+<a href="http://hackadelic.com/solutions/wordpress/sliding-notes#configuration">Nothing more to configure</a>.
+</p>
+<p>Cheers and happy sliding!</p>
+</div>
+<?php
+} //-- end function hackadelic_sliders_displayAdminPage
+
+?>
---
+++ readme.txt
@@ -0,0 +1,65 @@
+=== Hackadelic Sliding Notes ===
+Contributors: Hackadelic
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=1805414
+Tags: shortcode, AJAX, post, page, content, slide
+Requires at least: 2.6
+Tested up to: 2.7
+Stable tag: trunk
+
+Ajax sliders for content fragments. Fancy replacement for foot- and other notes.
+
+== Description ==
+
+Check out the [plug-in homepage](http://hackadelic.com/solutions/wordpress/sliding-notes "Sliding Notes") for in-depth information about the plug-in.
+
+== Installation ==
+
+1. Upload the whole plugin folder to your /wp-content/plugins/ folder.
+1. Go to the Plugins page and activate the plugin.
+1. Go to *Settings->Hackadelic Sliding Notes* and follow the instructions there.
+
+== Screenshots ==
+
+Screenshots? Who needs screenshots, when you can see it in live action at the [plug-in homepage](http://hackadelic.com/solutions/wordpress/sliding-notes "Sliding Notes")?
+
+== Change Log ==
+
+### 1.0rc5
+
+Fixes an issue with the print preview. (Notes were not initially collapsed in print preview. Reported by Mark Adams).
+
+### 1.0rc4, 1.0rc3
+
+Slight changes in the readme file only.
+
+### 1.0rc2
+
+Initial public release
+
+== License ==
+
+This file is part of the *Hackadelic Sliders* WordPress plugin.
+
+*Hackadelic Sliders* is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+*Hackadelic Sliders* is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with *Hackadelic Sliders*. If not, see <http://www.gnu.org/licenses/>.
+
+== Frequently Asked Questions ==
+
+= How do I use the plug-in? =
+
+Easy: Add the shortcode [slider title="slider button title"]slider note content[/slider] to your post or page (or in text widgets, if you enabled processing shortcodes there).
+
+= Why are there no options? =
+
+This was a deliberate decision, with the following reasoning:
+
+* For a typical blog site, the CSS definitions are unlikely to change, or change frequently, so it is basically a one-time effort for the administrator.
+* On the other hand, having extra options is an run-time overhead that occurrs on every page load, so it's an "every-time" penalty for each and every of your visitors.
+* Any customization I can think of right now is related to CSS styling. There already exists [a plug-in that allows for extra CSS styling from within the WordPress' admin interface - MyCSS](http://wordpress.org/extend/plugins/mycss/), so why re-invent the wheel, and add another stylesheet file to load on page displays?
+
+= I love your work, are you available for hire? =
+
+Yes I am. [Contact me](http://hackadelic.com/contact) to "talk turkey" :) 
