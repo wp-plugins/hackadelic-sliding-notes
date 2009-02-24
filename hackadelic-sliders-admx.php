65c61154b496327b8ebe74b7e6db7e4adcf4ae77
Hackadelic <Hackadelic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-24 15:15:32 +0000

    1.4.1a
    git-svn-id: https://plugins.svn.wordpress.org/hackadelic-sliding-notes/trunk@97233 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hackadelic-sliders-admx.php
+++ hackadelic-sliders-admx.php
@@ -35,8 +35,13 @@ $infomercials = array(
 		'text' => 'Contact the author',
 		'url' => 'http://hackadelic.com/contact',
 		'icon' => "http://lh3.ggpht.com/_eYaV9fZ6qRg/SYj7erc84II/AAAAAAAAAF8/NiwCQIS83Xs/s800/email_edit.png" ),
+
 	array(
 		'hr' => true,
+		'text' => 'All Hackadelic Stuff',
+		'url' => "http://wordpress.org/extend/plugins/profile/hackadelic",
+		'icon' => "http://lh5.ggpht.com/_eYaV9fZ6qRg/SZc9jB4lyiI/AAAAAAAAAHk/JTCZCfRxaG4/s800/shield.png" ),
+	array(
 		'text' => 'Subscribe',
 		'url' => 'http://hackadelic.com/feed',
 		'icon' => "http://lh4.ggpht.com/_eYaV9fZ6qRg/SYj7e2bE8DI/AAAAAAAAAGE/FoHbGZM2j3A/s800/feed.png" ),
@@ -91,4 +96,13 @@ You are using a <strong>Hackadelic PlugIn</strong>
 	<p><em><?php $this->e('Need an expert plugin developer?') ?>
 	 <a href="http://hackadelic.com/contact"><?php $this->e('Contact me') ?>!</a></em>
 	</p> */ ?>
+
+<hr size="0" />
+<center><small>
+<!-- Creative Commons License -->
+<a rel="nofollow" href="http://creativecommons.org/licenses/GPL/2.0/">
+<img alt="CC-GNU GPL" border="0" src="http://creativecommons.org/images/public/cc-GPL-a.png" /></a><br />
+This software is licensed under the <a href="http://creativecommons.org/licenses/GPL/2.0/">CC-GNU GPL</a> version 2.0 or later.
+<!-- /Creative Commons License -->
+</small></center>
 </div>
--- hackadelic-sliders.php
+++ hackadelic-sliders.php
@@ -2,7 +2,7 @@
 //---------------------------------------------------------------------------------------------
 /*
 Plugin Name: Hackadelic Sliding Notes
-Version: 1.4.0
+Version: 1.4.1a
 Plugin URI: http://hackadelic.com/solutions/wordpress/sliding-notes
 Description: Ajax sliders for content fragments
 Author: Hackadelic
@@ -25,7 +25,7 @@ function hackadelic_shortcode_slider_usage($atts, $content=null) {
 
 class HackadelicSliders
 {
-	var $VERSION = '1.4.0'; // Make sure this is equal to the one in the plug-in header!
+	var $VERSION = '1.4.1a'; // Make sure this is equal to the one in the plug-in header!
 
 	var $DEFAULT_TITLE = '+/-'; // Slider button title
 	var $TITLE_PREFX = 'expand/collapse slider: ';
@@ -39,6 +39,8 @@ class HackadelicSliders
 	//-------------------------------------------------------------------------------------
 
 	function initialize() {
+		//add_shortcode('dummy', array(&$this, 'doDummyShortcode'));
+
 		add_action('wp_print_scripts', array(&$this, 'enqueueScripts'));
 		add_action('wp_head', array(&$this, 'embedPrologue'), 99);
 		add_action('wp_footer', array(&$this, 'embedEpliogue'));
@@ -47,6 +49,11 @@ class HackadelicSliders
 		add_filter('the_content', array(&$this, 'postProcessContent'), 12);
 		// DO NOT preProcessContent with widget_text !
 		add_filter('widget_text', array(&$this, 'postProcessContent'), 12);
+	}
+	//-------------------------------------------------------------------------------------
+
+	function doDummyShortcode($atts, $content=null) {
+		return $content ? $content : '';
 	}
 
 	//-------------------------------------------------------------------------------------
@@ -115,7 +122,9 @@ class HackadelicSliders
 	//-------------------------------------------------------------------------------------
 
 	function postProcessContent($content) {
-		return $content . $this->notes;
+		$notes = $this->notes;
+		$this->notes = '';
+		return $content . $notes;
 	}
 
 	//-------------------------------------------------------------------------------------
