b9140d9ee49cf396a79d6476d0239036e2a1bb28
Hackadelic <Hackadelic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-12 17:17:49 +0000

    tagging 1.6.0
    git-svn-id: https://plugins.svn.wordpress.org/hackadelic-sliding-notes/tags/1.6.0@134486 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ common/license.AGPL.php
@@ -0,0 +1,3 @@
+<a rel="nofollow" href="http://www.fsf.org/licensing/licenses/agpl-3.0.html">
+		<img alt="AGPL" border="0" src="http://www.gnu.org/graphics/agplv3-155x51.png" /></a><br />
+		This software is licensed under the <a href="http://www.fsf.org/licensing/licenses/agpl-3.0.html">GNU Affero GPL</a> version 3.0 or later.
