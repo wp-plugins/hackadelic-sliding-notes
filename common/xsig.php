70835f4a5e5f927a879cf961c907863e15718309
Hackadelic <Hackadelic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-17 22:02:43 +0000

    tagging 1.6.2
    git-svn-id: https://plugins.svn.wordpress.org/hackadelic-sliding-notes/tags/1.6.2@136178 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ common/xsig.php
@@ -0,0 +1,4 @@
+<?php /** ?>
+<!-- <?php echo $plugin->title, ' ', $plugin->version ?>, http://hackadelic.com -->
+<div style="display:none">This site uses a Hackadelic PlugIn, <?php echo "$plugin->title $plugin->version" ?>.</div>
+<?php **/ ?><div style="display:none">This site uses a <a href="http://hackadelic.com">Hackadelic</a> PlugIn, <a href="http://hackadelic.com/solutions/wordpress/<?php echo $plugin->slug ?>"><?php echo "$plugin->title $plugin->version" ?></a>.</div>
